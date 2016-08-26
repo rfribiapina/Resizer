d02b329479d43626492fb133e820a94975e74c04
Renato Ibiapina <renatofri_ibi@hotmail.com>
2016-08-26 13:46:35 -0300

    Upgrade de funcionalidades

--- jquery.resizer.js
+++ jquery.resizer.js
@@ -6,52 +6,125 @@
 				_default = {
 					resizeOrientation: 'all',
 					aspectRatioLocked: true,
-					callback: function(){},
-					dragCallback: function(){}
+					callback: function(){}
 				},
-				resizeW = function(elm, left){
-					var nw = elm.width() + ( left - elm.width() + 5);
+				resizeT = function(elm, top){
+					var nt = Math.abs(__top + top);
+						nh = Math.abs(__height - top);
+					elm.css({ height: nh, top: nt });
+				},
+				resizeR = function(elm, left){
+					var nw = elm.width() + ( left - elm.width());
 					elm.css({ width: nw });
 				},
-				resizeH = function(elm, top){
-					var nh = elm.height() + ( top - elm.height() + 5);
+				resizeB = function(elm, top){
+					var nh = elm.height() + ( top - elm.height());
 					elm.css({ height: nh });
-				};
+				},
+				resizeL = function(elm, left){
+					var nl = Math.abs(__left + left);
+						nw = Math.abs(__width - left);
+					elm.css({ width: nw, left: nl });
+				},
+				__width = $elm.width(),
+				__height = $elm.width(),
+				__left = $elm[0].offsetLeft,
+				__top = $elm[0].offsetTop;
+				
 			_options = $.extend(true, {}, _default, args);
 			
-			switch(_options.resizeOrientation){
-				case 'w': $elm.append('<span class="rszW"></span>'); break;
-				case 'h': $elm.append('<span class="rszH"></span>'); break;
-				case 'all': $elm.append('<span class="rszW"></span><span class="rszH"></span>'); break;
-			}
-			$elm.draggable({ cursor: "move", cancel: ".rszW, .rszH", drag: _options.dragCallback });	
-				
-			$elm.children('.rszW').draggable({ 
-				axis: "x",
-				drag: function(e,ui){
-					resizeW($elm, ui.position.left);
-					if( _options.aspectRatioLocked )
-						resizeH($elm, ui.position.left);
-				},
-				stop: function(e,ui){					
-					if( typeof(_options.callback) == 'function' ){
-						_options.callback($elm,$elm.width(),$elm.height());
-					}
+			$elm.append('<span class="rszT" disabled="true"></span><span class="rszR" disabled="true"></span><span class="rszB" disabled="true"></span><span class="rszL" disabled="true"></span>');
+			
+			var roArr = _options.resizeOrientation.split(',');			
+			for(var k in roArr)
+			{	
+				switch(roArr[k]){
+					case 't': $elm.children('.rszT').removeAttr('disabled'); break;
+					case 'r': $elm.children('.rszR').removeAttr('disabled'); break;
+					case 'b': $elm.children('.rszB').removeAttr('disabled'); break;
+					case 'l': $elm.children('.rszL').removeAttr('disabled'); break;
+					case 'all': $elm.children('.rszT, .rszR, .rszB, .rszL').removeAttr('disabled'); break;
 				}
-			});		
-			$elm.children('.rszH').draggable({ 
+			}
+			$elm.draggable({ cursor: "move", cancel: ".rszT, .rszR, .rszB, .rszL" });	
+					
+			$elm.children('.rszT').not("[disabled=true]").draggable({ 
 				axis: "y",
-				drag: function(e,ui){
-					resizeH($elm, ui.position.top);
-					if( _options.aspectRatioLocked )
-						resizeW($elm, ui.position.top);
+				start: function(e,ui){ 
+					Functions.dndStartFunction(ui);
+					__height = $elm.height();
+					__top = $elm[0].offsetTop;
 				},
-				stop: function(e,ui){					
+				drag: function(e,ui){
+					Functions.dndDragFunction(ui);
+					resizeT($elm, ui.position.top);
+					if( _options.aspectRatioLocked )
+						resizeR($elm, ui.position.top);
+					ui.position.top = 0;
+					$elm.children('.rszL').attr('style','');
+				},
+				stop: function(e,ui){				
+					$elm.children('.rszT').attr('style','');
 					if( typeof(_options.callback) == 'function' ){
-						_options.callback($elm,$elm.width(),$elm.height());
+						_options.callback($elm,$elm.width(),$elm.height(),$elm[0].offsetTop,$elm[0].offsetLeft);
 					}
 				}
 			});	
+			$elm.children('.rszR').not("[disabled=true]").draggable({ 
+				axis: "x",
+				start: function(e,ui){ Functions.dndStartFunction(ui); },
+				drag: function(e,ui){
+					Functions.dndDragFunction(ui);
+					resizeR($elm, ui.position.left);
+					if( _options.aspectRatioLocked )
+						resizeB($elm, ui.position.left);
+				},
+				stop: function(e,ui){			
+					$elm.children('.rszR').attr('style','');		
+					if( typeof(_options.callback) == 'function' ){
+						_options.callback($elm,$elm.width(),$elm.height(),$elm[0].offsetTop,$elm[0].offsetLeft);
+					}
+				}
+			});		
+			$elm.children('.rszB').not("[disabled=true]").draggable({ 
+				axis: "y",
+				start: function(e,ui){ Functions.dndStartFunction(ui); },
+				drag: function(e,ui){
+					Functions.dndDragFunction(ui);
+					resizeB($elm, ui.position.top);
+					if( _options.aspectRatioLocked )
+						resizeR($elm, ui.position.top);
+				},
+				stop: function(e,ui){		
+					$elm.children('.rszB').attr('style','');			
+					if( typeof(_options.callback) == 'function' ){
+						_options.callback($elm,$elm.width(),$elm.height(),$elm[0].offsetTop,$elm[0].offsetLeft);
+					}
+				}
+			});	
+			$elm.children('.rszL').not("[disabled=true]").draggable({ 
+				axis: "x",
+				start: function(e,ui){ 
+					Functions.dndStartFunction(ui);
+					__width = $elm.width();
+					__left = $elm[0].offsetLeft;
+				},
+				drag: function(e,ui){
+					Functions.dndDragFunction(ui);
+					resizeL($elm, ui.position.left);
+					if( _options.aspectRatioLocked )
+						resizeB($elm, ui.position.left);
+					ui.position.left = 0;
+					$elm.children('.rszL').attr('style','');
+				},
+				stop: function(e,ui){	
+					$elm.children('.rszL').attr('style','');
+					if( typeof(_options.callback) == 'function' ){
+						_options.callback($elm,$elm.width(),$elm.height(),$elm[0].offsetTop,$elm[0].offsetLeft);
+					}
+				}
+			});	
+			
         });
     }
 })(jQuery);
